1b95c98f21a22d8b5cad3a89d77e87dc056e1cee
wsu2059q <wsu2059@qq.com>
2026-09-12 14:22:20 +0800

    fix(qqbot): _reset_modifiers 清理基类 _keyboard_rows

--- QQBotAdapter/Core.py
+++ QQBotAdapter/Core.py
@@ -403,6 +403,7 @@ class QQBotAdapter(sdk.BaseAdapter):
             self._reply_message_id = None
             self._at_all = False
             self._keyboard = None
+            self._keyboard_rows = None
 
         def Raw_ob12(self, message, **kwargs):
             """
